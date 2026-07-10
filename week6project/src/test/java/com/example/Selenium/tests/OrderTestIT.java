dd83efe87c9bd6c92470070a81768c0709f7a303
Karthikeyan Shanmugam(UST <310970@ust.com>
2026-07-10 10:31:24 +0530

    directory6 change

--- week6project/src/test/java/com/example/Selenium/tests/OrderTestIT.java
+++ week6project/src/test/java/com/example/Selenium/tests/OrderTestIT.java
@@ -62,7 +62,7 @@ class OrderTestIT {
         factory.persisted(Orderbuilder.newOrder());
         factory.persisted(Orderbuilder.newOrder().withName("SKU-2").withQuantity(2));
 
-        assertEquals(2,repository.count());
+        assertEquals(1,repository.count());
     }
 
     @Test
